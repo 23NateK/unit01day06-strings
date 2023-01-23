e2ab15fea4660b113824e5a28e22cab921a34d8c
23NateK <n.kimber22@gmail.com>
2023-01-23 11:52:05 -0600

    Finished NegatePhrase

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,5 +1,10 @@
 public class NegatePhrase {
     public static void main(String[] args) {
-        
+        String phrase = args[0];
+        if (phrase.substring(0,3).equals("not")){
+            System.out.println(phrase);
+        }else{
+            System.out.println("not "+ phrase);
+        }
     }
 }
