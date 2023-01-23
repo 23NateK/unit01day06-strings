1e45be6b1b819078343f2d3cde37662f5ad14053
23NateK <n.kimber22@gmail.com>
2023-01-23 11:44:25 -0600

    added else and result, finished Abbreviate

--- Abbreviate.java
+++ Abbreviate.java
@@ -3,6 +3,8 @@ public class Abbreviate {
         String word = args[0];
         if (word.length()<8){
             System.out.println(word+" | "+word);
+        }else{
+            System.out.println(word+" | "+word.substring(0,2)+"..."+word.substring(word.length()-2,word.length()));
         }
     }
 }
