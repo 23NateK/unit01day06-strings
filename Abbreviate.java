7993f293a93625ff486d7810b6ab07c8462509c3
23NateK <n.kimber22@gmail.com>
2023-01-23 11:49:48 -0600

    fixed printout

--- Abbreviate.java
+++ Abbreviate.java
@@ -2,9 +2,9 @@ public class Abbreviate {
     public static void main(String[] args) {
         String word = args[0];
         if (word.length()<8){
-            System.out.println(word+" | "+word);
+            System.out.println(word);
         }else{
-            System.out.println(word+" | "+word.substring(0,2)+"..."+word.substring(word.length()-2,word.length()));
+            System.out.println(word.substring(0,2)+"..."+word.substring(word.length()-2,word.length()));
         }
     }
 }
