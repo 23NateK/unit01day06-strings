72e0e99fa0940752a9fa90e205df023d89bd5d6d
23NateK <n.kimber22@gmail.com>
2023-01-23 11:57:20 -0600

    Fixed ShortLongShort

--- ShortLongShort.java
+++ ShortLongShort.java
@@ -3,9 +3,11 @@ public class ShortLongShort {
         String word1 = args[0];
         String word2 = args[1];
         if (word1.length()>word2.length()){
-            System.out.println(word2+word1+word2);
+            String newword = word2 + word1 + word2;
+            System.out.println(newword);
         }else{
-            System.out.println(word1+word2+word1);
+            String newword = word1 + word2 + word1;
+            System.out.println(newword);
         }
     }
 }
