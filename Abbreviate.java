c318c9a12a69d27f7d35e6064b8abd0495d53060
23NateK <n.kimber22@gmail.com>
2023-01-23 11:38:03 -0600

    added check if less than 8 and result

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,8 @@
 public class Abbreviate {
     public static void main(String[] args) {
-        
+        String word = args[0];
+        if (word.length()<8){
+            System.out.println(word+" | "+word);
+        }
     }
 }
